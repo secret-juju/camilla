d844e56bd3f005c9b39c6672c20f34df160ed22c
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-29 18:06:06 +0900

    add method that findByDateAndCompanyId.

--- src/main/kotlin/com/dsm/camillia/domain/stock/repository/StockRepository.kt
+++ src/main/kotlin/com/dsm/camillia/domain/stock/repository/StockRepository.kt
@@ -2,5 +2,8 @@ package com.dsm.camillia.domain.stock.repository
 
 import com.dsm.camillia.domain.stock.domain.Stock
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDate
 
-interface StockRepository : JpaRepository<Stock, Long>
+interface StockRepository : JpaRepository<Stock, Long> {
+    fun findByDateAndCompanyId(date: LocalDate, companyId: Long): Stock?
+}
