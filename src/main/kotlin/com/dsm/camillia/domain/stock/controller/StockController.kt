0a9ab69e094b16b725f0bbdd1745821509063f22
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-29 13:29:44 +0900

    add stock controller.

--- src/main/kotlin/com/dsm/camillia/domain/stock/controller/StockController.kt
+++ src/main/kotlin/com/dsm/camillia/domain/stock/controller/StockController.kt
@@ -1,4 +1,25 @@
 package com.dsm.camillia.domain.stock.controller
 
-class StockController {
+import com.dsm.camillia.domain.stock.controller.request.StockInitializationRequest
+import com.dsm.camillia.global.crawler.StockCrawler
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+class StockController(
+    private val stockCrawler: StockCrawler,
+) {
+
+    @PostMapping("/initialization")
+    fun initializeStock(
+        @Valid
+        @RequestBody
+        request: StockInitializationRequest,
+    ) {
+        request.tickerSymbol.forEach {
+            stockCrawler.stockInformationCrawling(it)
+        }
+    }
 }
