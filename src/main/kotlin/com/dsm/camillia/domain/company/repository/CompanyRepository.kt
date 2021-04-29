6e93e9543e7baf20ab303155c986e263d2ac12a9
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-29 14:47:08 +0900

    create repository that company search function.

--- src/main/kotlin/com/dsm/camillia/domain/company/repository/CompanyRepository.kt
+++ src/main/kotlin/com/dsm/camillia/domain/company/repository/CompanyRepository.kt
@@ -1,4 +1,8 @@
 package com.dsm.camillia.domain.company.repository
 
-interface CompanyRepository {
+import com.dsm.camillia.domain.company.domain.Company
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CompanyRepository : JpaRepository<Company, Long> {
+    fun findByTickerSymbol(tickerSymbol: String): Company?
 }
