c5dbb730ac0e69973051e8bb33a2b9b900d6a449
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-29 18:07:39 +0900

    add test controller.

--- src/main/kotlin/com/dsm/camillia/domain/stock/controller/StockController.kt
+++ src/main/kotlin/com/dsm/camillia/domain/stock/controller/StockController.kt
@@ -2,6 +2,7 @@ package com.dsm.camillia.domain.stock.controller
 
 import com.dsm.camillia.domain.stock.controller.request.StockInitializationRequest
 import com.dsm.camillia.global.crawler.StockCrawler
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -19,7 +20,12 @@ class StockController(
         request: StockInitializationRequest,
     ) {
         request.tickerSymbol.forEach {
-            stockCrawler.stockInformationCrawling(it)
+            stockCrawler.crawlingStockInformation(it)
         }
+    }
+
+    @GetMapping("/")
+    fun test() {
+        stockCrawler.crawlingTodayStockInformation("005930")
     }
 }
