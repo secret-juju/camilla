3e8f493c8b82d1ed71bbec27cf9428b47f23af1e
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-30 09:40:04 +0900

    add method that search every company.

--- src/main/kotlin/com/dsm/camillia/domain/company/service/CompanySearchService.kt
+++ src/main/kotlin/com/dsm/camillia/domain/company/service/CompanySearchService.kt
@@ -1,5 +1,6 @@
 package com.dsm.camillia.domain.company.service
 
+import com.dsm.camillia.domain.company.domain.Company
 import com.dsm.camillia.domain.company.exception.CompanyNotFoundException
 import com.dsm.camillia.domain.company.repository.CompanyRepository
 import org.springframework.stereotype.Service
@@ -12,4 +13,6 @@ class CompanySearchService(
     fun getCompanyByTickerSymbol(tickerSymbol: String) =
         companyRepository.findByTickerSymbol(tickerSymbol)
             ?: throw CompanyNotFoundException(tickerSymbol)
+
+    fun getAllCompany(): List<Company> = companyRepository.findAll()
 }
