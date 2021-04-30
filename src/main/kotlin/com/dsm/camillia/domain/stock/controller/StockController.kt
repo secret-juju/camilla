bd28db9e4698b0f96295cefd90c0d738e9ae55e9
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-04-30 09:39:35 +0900

    modify URL from [GET] /ticker-symbol/{tickerSymbol} to [PUT] /initialization/today.

--- src/main/kotlin/com/dsm/camillia/domain/stock/controller/StockController.kt
+++ src/main/kotlin/com/dsm/camillia/domain/stock/controller/StockController.kt
@@ -1,30 +1,28 @@
 package com.dsm.camillia.domain.stock.controller
 
-import com.dsm.camillia.domain.stock.controller.request.StockInitializationRequest
+import com.dsm.camillia.domain.company.service.CompanySearchService
 import com.dsm.camillia.global.crawler.StockCrawler
 import org.springframework.web.bind.annotation.*
-import javax.validation.Valid
 
 @RestController
 class StockController(
     private val stockCrawler: StockCrawler,
+    private val companySearchService: CompanySearchService,
 ) {
 
     @PostMapping("/initialization")
-    fun initializeStock(
-        @Valid
-        @RequestBody
-        request: StockInitializationRequest,
-    ) {
-        request.tickerSymbol.forEach {
-            stockCrawler.crawlingStockInformation(it)
-        }
-    }
+    fun initializeStock() =
+        companySearchService.getAllCompany()
+            .map { it.tickerSymbol }
+            .forEach {
+                stockCrawler.crawlingStockInformation(it)
+            }
 
-    @GetMapping("/ticker-symbol/{tickerSymbol}")
-    fun test(
-        @PathVariable("tickerSymbol") tickerSymbol: String
-    ) {
-        stockCrawler.crawlingTodayStockInformation(tickerSymbol)
-    }
+    @PutMapping("/initialization/today")
+    fun modifyTodayStock() =
+        companySearchService.getAllCompany()
+            .map { it.tickerSymbol }
+            .forEach {
+                stockCrawler.crawlingTodayStockInformation(it)
+            }
 }
