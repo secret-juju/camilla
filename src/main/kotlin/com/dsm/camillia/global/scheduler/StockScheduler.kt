244525626f3731654d7af8ff7f872ee62ec04b52
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2021-05-18 09:20:01 +0900

    modify scheduling logic.

--- src/main/kotlin/com/dsm/camillia/global/scheduler/StockScheduler.kt
+++ src/main/kotlin/com/dsm/camillia/global/scheduler/StockScheduler.kt
@@ -2,7 +2,6 @@ package com.dsm.camillia.global.scheduler
 
 import com.dsm.camillia.domain.company.service.CompanySearchService
 import com.dsm.camillia.global.crawler.StockCrawler
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
